8346eb5e1afa2667c02514c03ea27f2bfe29ab85
Judy Essam <107353757+juejue543@users.noreply.github.com>
2024-03-16 01:30:32 +0200

    Final Draft

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
-
-  
+document.querySelector('.hamburger-button').addEventListener('click', function() {
+  document.querySelector('.nav-links').style.display = 
+  (document.querySelector('.nav-links').style.display == 'none') ? 'block' : 'none';
+});
